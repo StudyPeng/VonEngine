f7a81ac5a2dbd85442111afcbe07c4878b83c1d0
Tonny Peng <studytonny@gmail.com>
2026-07-08 22:53:07 +0800

    init vg

---
+++ src/vg.cpp
@@ -0,0 +1,29 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <vulkan/vulkan.h>
+
+class TriangleApp {
+public:
+  void run() {
+    init();
+    loop();
+    cleanup();
+  }
+
+private:
+  void init() {}
+  void loop() {}
+  void cleanup() {}
+};
+
+int main() {
+  TriangleApp app;
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
